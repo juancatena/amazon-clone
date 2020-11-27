28ced99fb3d1e44a47cafe186a2767ea34e1c01b
juancatena <juancatena4@hotmail.com>
2020-11-27 08:49:42 -0300

    version 1

---
+++ build/precache-manifest.8a1d8e681b121dbbc35fbd45837d63be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1859587d48fab38f13b16282ab7d11ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec3d54b7628f51d3faab",
+    "url": "/static/css/main.3e31a5ee.chunk.css"
+  },
+  {
+    "revision": "d1e36a7a03838d4f6290",
+    "url": "/static/js/2.90e84671.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.90e84671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3d54b7628f51d3faab",
+    "url": "/static/js/main.8a4b3a66.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
